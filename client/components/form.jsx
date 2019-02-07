91ceba498c21bca288af0e27290d09c561c58821
mijucho <chomiju38@gmail.com>
2019-02-08 10:57:16 +1300

    mofied Form

--- client/components/form.jsx
+++ client/components/form.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 
-class From extends React.Component {
+class Form extends React.Component {
     constructor(props) {
         super(props)
     }
 
   render(){
+      return(
       <form>
           <label>Country: <input type="text" name="country" /></label>
           <label>City: <input type="text" name="city"/></label>
           <button>Submit</button>
-      </form>    
+      </form>  
+      )  
   }  
 }
 
